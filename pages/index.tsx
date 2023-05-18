13fb66e3657accfa944f65cf3a34d84498930914
Ali <alioliaee31@gmail.com>
2023-05-18 23:57:24 +0330

    create page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,61 @@
-import {BottomRightGradient, Container, Glass, TopLeftGradient} from '@styles/index'
+import {BottomRightGradient, 
+  Container,
+  Content,
+  Description,
+  Glass,
+  Header,
+  InfoContainer,
+  LogoName,
+  StartButton,
+  Title,
+  TopLeftGradient
+} from '@styles/index'
+import Image from 'next/image'
 
 const HomePage: React.FC = ()  =>{
   return (
-   <Container>
-    <TopLeftGradient/>
-    <Glass>
-      <div className="header"></div>
-      <div className="content">
-        <div className="image"></div>
-        <div className="info-container">
-          <div className="title"></div>
-          <div className="description"></div>
-        </div>
-      </div>
-        </Glass>
-        <BottomRightGradient/>
+    <Container>
+      <TopLeftGradient/>
+      <Glass>
+        <Header>
+          <LogoName>Ali</LogoName>
+          <div className="menu">
+          EN
+            <Image
+              src="/Moon.svg"
+              alt="moon"
+              width={30}
+              height={30}
+            />
+          </div>
+        </Header>
+        <Content>
+          <InfoContainer>
+            <Title>
+              Hi! I’m Ali
+            </Title>
+            <Title>
+              Mobile and Web Developer
+            </Title>
+            <Description>
+            Welcome to my portfolio!
+            I&apos;m a mobile and frontend developer with expertise
+            in iOS, Android, and React.
+            My goal is to create beautiful and functional applications
+            that exceed my client&apos;s expectations. Let&apos;s work together 
+            to bring your ideas to life!
+            </Description>
+            <StartButton>Start a project</StartButton>
+          </InfoContainer>
+          <Image
+            src="/FloatPerson.svg"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          />
+        </Content>
+      </Glass>
+      <BottomRightGradient/>
     </Container>
   )
 }
--- styles/index.tsx
+++ styles/index.tsx
@@ -8,46 +8,92 @@ export const Container = styled.div`
     height: 100vh;
     background-color: ${Colors.background};
 `
-
 export const Glass = styled.div`
+    background-clip: padding-box;
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
-    background-color: rgba( 255, 255, 255, 0.10 );
+    justify-content: flex-start;
     width: 90%;
     height: 80%;
-    backdrop-filter: blur(1px);
-    border-image: linear-gradient(to right, rgba( 255, 255, 255, 0.5 ), rgba( 255, 255, 255, 0.10 )) 4;
-    border-radius: 20px;
-    border-width: 4px;
-    border-style: solid;
-    z-index: 2;
-    background-color: rgba(255, 255, 255, 0.1);
+    border-image: 
+        linear-gradient(to right,
+             rgba( 255, 255, 255, 0.5 ),
+              rgba( 255, 255, 255, 0.10 )) 4;
+    border-radius: 30px;
+    border-width: 2px;
     backdrop-filter: blur(10px) brightness(120%) saturate(120%);
+    border-style: solid;
     opacity: 0.8;
 `
-
 export const TopLeftGradient = styled.div`
     position: absolute;
     top: -40px;
     left: -40px;
-    width: 20%;
-    filter: blur(120px);
-    height: 30%;
+    width: 25%;
+    filter: blur(100px);
+    height: 35%;
+    background: linear-gradient( #00C2FF 0%, #FF29C3 80%);
+    border-radius: 50%;
+    z-index: 1;
+`
+export const BottomRightGradient = styled.div`
+    position: absolute;
+    bottom: -40px;
+    right: -40px;
+    width: 15%;
+    filter: blur(100px);
+    height: 25%;
     background: linear-gradient( #00C2FF 0%, #FF29C3 100%);
     border-radius: 50%;
     z-index: 1;
 `
-        
-export const BottomRightGradient = styled.div`
-    position: absolute;
-    bottom: -40px;
-    tight: -40px;
-    width: 20%;
-    filter: blur(120px);
-    height: 30%;
-    background: linear-gradient( #00C2FF 0%, #FF29C3 100%);
-    border-radius: 50%;
-    z-index: 1;
+export const Header = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    width: 100%;
+    height: 10%;
+    z-index: 2;
+`
+export const LogoName = styled.h1`
+    font-size: 1.5rem;
+    color: #fff;
+    font-weight: 700;
+`
+export const Content = styled.div` 
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+export const InfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+`
+export const Title = styled.h1`
+    font-size: 2rem;
+    color: #fff;
+    font-weight: 700;
+`
+export const Description = styled.p`
+    font-size: 1.5rem;
+    color: #fff;
+    font-weight: 400;
+`
+export const StartButton = styled.button`
+    background-color: red;
+    color: #fff;
+    font-size: 1.5rem;
+    font-weight: 700;
+    border-radius: 10px;
+    padding: 10px;
+    border: none;
+    cursor: pointer;
+    transition: 0.3s;
+    &:hover{
+        background-color: green;
+    }
 `
