87326a0418fbfaac346ee25e0201bebb76e4140f
Ali <alioliaee31@gmail.com>
2023-06-02 13:26:45 +0330

    apply fonts

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,7 +1,19 @@
+import {Vazirmatn} from '@next/font/google'
+
 export const metadata = {
   title: 'Ali Oliaee | علی علیایی',
   description: 'personal website',
+  creator: 'Ali Oliaee',
+  publisher: 'Ali Oliaee',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
+
+const vazirmatn = Vazirmatn({
+  weight : ['400','700'],
+  subsets: ['arabic', 'latin-ext'] 
+})
 
 export default function RootLayout({
   children,
@@ -10,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={vazirmatn.className}>{children}</body>
     </html>
   )
 }
