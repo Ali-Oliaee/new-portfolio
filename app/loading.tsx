2ca00854e2b37ec2cd18696d12ff2bc1979441e4
Ali <alioliaee31@gmail.com>
2023-06-02 12:08:55 +0330

    move pages to app dir

---
+++ app/loading.tsx
@@ -0,0 +1,16 @@
+const Loading = () => {
+    return(
+        <div style={{
+            minHeight: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            backgroundColor: 'red',
+        }}>
+        <h3>please wait...</h3>
+        </div>
+    )
+}
+
+export default Loading
