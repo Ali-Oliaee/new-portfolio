677c5c5513e06fa0f9d266915e4ebd62dce404bc
Ali <alioliaee31@gmail.com>
2023-06-02 13:34:14 +0330

    refactor

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -1,8 +1,6 @@
 // import 'server-only'
 import type { Locale } from './next-i18next.config'
 
-// We enumerate all dictionaries here for better linting and typescript support
-// We also get the default import for cleaner types
 const dictionaries = {
   en: () => import('./dictionaries/en.json').then((module) => module.default),
   fa: () => import('./dictionaries/fa.json').then((module) => module.default),
