81fcea42e1713c7b0bfc46d9fddd9b967c2f4d83
Ali <alioliaee31@gmail.com>
2023-06-04 22:10:49 +0330

    add styles

--- app/[lang]/components/footer/index.tsx
+++ app/[lang]/components/footer/index.tsx
@@ -1,7 +1,23 @@
+import './styles.sass'
+import { SocialMedias } from '..'
+
 export default async function Footer({lang}:LanguageProp){
   return (
     <footer>
-      <p>Footer</p>
+      <div className="footer-content">
+        <div className="about">
+          <div className="general">
+            <h3>Ali Oliaee</h3>
+            <span className='email'>alioliaee31@gmail.com</span>
+          </div>
+            mobile and web developer
+        </div>
+        <div className="social">
+          <h4>Social Medias</h4>
+          <SocialMedias/>
+        </div>
+      </div>
+      <p className="copyright">© Copyright 2023. Made by Ali.</p>
     </footer>
   )
 }
--- app/[lang]/home/page.tsx
+++ app/[lang]/home/page.tsx
@@ -1,7 +1,12 @@
-import {Header} from '../components'
+import {Footer, Header} from '../components'
 
 const HomePage: React.FC = ({params:{lang}}:LanguageParam) => {
-  return <Header lang={lang}/>
+  return (
+    <>
+      <Header lang={lang}/>
+      <Footer lang={lang}/>
+    </>
+  )
 }
 
 export default HomePage
