b3ae1ae12f312e06555207a9d3ac1d9b8acf2aa8
Ali <alioliaee31@gmail.com>
2023-06-04 22:14:54 +0330

    add translations

--- app/[lang]/components/footer/index.tsx
+++ app/[lang]/components/footer/index.tsx
@@ -1,23 +1,26 @@
 import './styles.sass'
 import { SocialMedias } from '..'
+import { getDictionary } from 'get-dictionary'
 
 export default async function Footer({lang}:LanguageProp){
+  const dictionary = await getDictionary(lang)
+
   return (
-    <footer>
+    <footer dir={lang === 'en' ? 'ltr' : 'rtl'}>
       <div className="footer-content">
         <div className="about">
           <div className="general">
-            <h3>Ali Oliaee</h3>
+            <h3>{dictionary.fullName}</h3>
             <span className='email'>alioliaee31@gmail.com</span>
           </div>
-            mobile and web developer
+          {dictionary.role}
         </div>
         <div className="social">
-          <h4>Social Medias</h4>
+          <h4>{dictionary.socialTitle}</h4>
           <SocialMedias/>
         </div>
       </div>
-      <p className="copyright">© Copyright 2023. Made by Ali.</p>
+      <p className="copyright">© Copyright 2023. Made by Ali</p>
     </footer>
   )
 }
