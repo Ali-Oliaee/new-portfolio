ce890716e12cecb996cd061c48b1f0587ba54f92
Ali <alioliaee31@gmail.com>
2023-06-05 11:17:22 +0330

    add more responsive

--- app/[lang]/sections/header-section/index.tsx
+++ app/[lang]/sections/header-section/index.tsx
@@ -1,23 +1,35 @@
-import { getDictionary } from 'get-dictionary'
 import './styles.sass'
+import Image from 'next/image'
+import { getDictionary } from 'get-dictionary'
 
 async function HeaderSection({lang}: LanguageProp) {
   const dictionary = await getDictionary(lang)
 
   return (
-    <div className="container">
+    <div
+      className="container" 
+      dir={lang === 'fa' ? 'rtl' : 'ltr'}
+    >
       <div className="info-container">
-        <h4 className="name">{dictionary.headerSectionTitle}</h4>
+        <h2 className="title">{dictionary.headerSectionTitle}</h2>
         <p className="description">{dictionary.headerSectionDescription}</p>
-        <button className="contact-button">
-          {dictionary.contactMe}
-        </button>
+        <div className="footer">
+          <button className="contact-button">
+            {dictionary.contactMe}
+          </button>
+        </div>
       </div>
       <div className="image-container">
-        {/* <img src="/images/ali-oliaee.jpg" alt="ali oliaee" /> */}
+        <Image
+          src="/me.jpeg"
+          alt="Ali Oliaee"
+          width={300}
+          height={300}
+          className='image'
+        />
         <div className="status-box">
+          <div className="status-icon"/>
           <p className="status">{dictionary.workStatus}</p>
-          <div className="status-icon"></div>
         </div>
       </div>
     </div>
